c2a660ef5dd5008d92921f02eee1d1dd2442a2df
unknown <Heber@Heber-Desktop.(none)>
2011-12-12 10:45:00 -0200

    ajax on delete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zombies2::Application.config.secret_token = 'c43837ccfee370637345a4c27c471bd391b04bd9bc775bca14c0da14db4ee22a36c5c8e287025c3102552504d94d9b10a1e14ee1a9a3a9ce1ed0e3ec8ed4b5ea'
